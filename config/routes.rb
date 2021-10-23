38f2a7b594b5b25cd05fab6fc6bcdfb39920bc82
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-10-23 14:57:49 +0900

    generate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :enquetes
   resources :teams, only: [:index, :show]
   root to: "home#index"
 end
---
+++ test/controllers/enquetes_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class EnquetesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get enquetes_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get enquetes_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get enquetes_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get enquetes_edit_url
+    assert_response :success
+  end
+end
