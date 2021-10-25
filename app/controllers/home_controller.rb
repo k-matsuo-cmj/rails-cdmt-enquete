d9e03da66e350d48fe05e20e2de6fc33cd1a91f7
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-10-26 07:52:05 +0900

    indexをHomeに移動

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,12 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @enquetes = Enquete.where(team: managed_teams)
+    @replies = Reply.where(user: current_user)
   end
+
+  private
+    def managed_teams
+      Team.where(manager: current_user)
+    end
 end
