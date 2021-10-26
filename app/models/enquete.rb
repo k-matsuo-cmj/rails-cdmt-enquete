841c820a67f9e7f6782beadefda1282e79668f7e
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-10-26 20:06:42 +0900

    ソート順を降順にする

--- app/models/enquete.rb
+++ app/models/enquete.rb
@@ -1,6 +1,7 @@
 class Enquete < ApplicationRecord
   belongs_to :sender, class_name: "User"
   has_many :replies, dependent: :destroy
+  default_scope { order(created_at: :desc) }
 
   validates :title, presence: true
   validates :deadline, presence: true
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,5 @@
 class Reply < ApplicationRecord
   belongs_to :enquete
   belongs_to :user
+  default_scope { order(created_at: :desc) }  
 end
