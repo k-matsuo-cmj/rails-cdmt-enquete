281c329c403e3c7b9a2871221acceaf208720c2c
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-10-27 22:28:00 +0900

    CSV出力追加

--- app/controllers/enquetes_controller.rb
+++ app/controllers/enquetes_controller.rb
@@ -1,10 +1,15 @@
 class EnquetesController < ApplicationController
+  require 'csv'
   before_action :authenticate_user!
   before_action :manager_user
   before_action :sender_user, only: [:show, :edit, :update, :delete]
 
   def show
     @enquete = Enquete.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.csv {send_csv(@enquete)}
+    end
   end
 
   def new
@@ -72,4 +77,31 @@ class EnquetesController < ApplicationController
         redirect_to root_url
       end
     end
+
+    # CSV出力処理
+    def send_csv(enquete)
+      bom = "\uFEFF"
+      data = CSV.generate(bom, force_quotes: true) do |csv|
+        csv << CSV_COLUMNS
+        enquete.replies.each do |reply|
+          csv << reply.to_csv
+        end
+      end
+      send_data(data, filename: "enqreply_#{Time.current.strftime("%Y%m%d%H%M%S")}.csv")
+    end
+
+    # CSVヘッダカラム
+    CSV_COLUMNS = %w(
+      氏名 回答日時 社員番号 グレード ステータス
+      PJ_テクニカル・スキル PJ_人間関係 PJ_アピール・問題点
+      希望職種 1年後_グレード 1年後_人材イメージ 1年後_行動・目標
+      3年後_グレード 3年後_人材イメージ 3年後_行動・目標
+      5年後_グレード 5年後_人材イメージ 5年後_行動・目標
+      現_CD目標① 現_進捗率① 現_進捗状況① 現_弊害・問題点①
+      現_CD目標② 現_進捗率② 現_進捗状況② 現_弊害・問題点②
+      次_CD目標① 次_分配率① 次_CD目標② 次_分配率②
+      受講日・セミナー名 受講時間合計
+      業務知識 顧客評価・CSサービス 会社貢献度 仕事の量・質 積極性 規律性 責任性
+      上長マネージャに対する要望 営業部に対する要望 管理部に対する要望 会社に対する要望
+    )
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,4 +1,57 @@
 class Reply < ApplicationRecord
   belongs_to :enquete
   belongs_to :user
+
+  def to_csv
+    [
+      user.user_name,
+      format_datetime(submitted_at),
+      user.profile.emp_no,
+      this_grade,
+      this_status,
+      pj_technical_skill,
+      pj_relationships,
+      pj_appeal_problem,
+      future_job_category,
+      future_grade_1y,
+      future_image_1y,
+      future_action_1y,
+      future_grade_3y,
+      future_image_3y,
+      future_action_3y,
+      future_grade_5y,
+      future_image_5y,
+      future_action_5y,
+      this_target1,
+      this_target1_achv,
+      this_target1_achv,
+      this_target1_remarks,
+      this_target2,
+      this_target2_achv,
+      this_target2_achv,
+      this_target2_remarks,
+      next_target1,
+      next_target1_ratio,
+      next_target2,
+      next_target2_ratio,
+      seminor_contents,
+      course_hour,
+      eval_knowledge,
+      eval_customer,
+      eval_contribution,
+      eval_quantity_quality,
+      eval_posivity,
+      eval_decipline,
+      eval_responsibility,
+      req_manager,
+      req_sales,
+      req_admin,
+      req_company
+    ]
+  end
+
+  private
+    def format_datetime(datetime)
+      datetime.strftime("%Y/%m/%d %H:%M:%S") unless datetime.nil?
+    end
 end
